4951ef6c46e794fb76946b34188ca37d428ca3ec
rh8512 <ppenza@gmail.com>
2021-04-02 16:08:10 +0200

    Interpreter Design Pattern

---
+++ DesignPatterns/DesignPatterns.InterpreterDesignPattern/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+Appointment appointment = new() { IsVip = true, PatientBirthDate = DateTime.Now };
+
+List<IValidation> validations = new List<IValidation>()
+{
+    new IsVipValidation(),
+    new IsAdultValidation()
+};
+
+foreach(var validation in validations)
+{
+    var result = validation.Validate(appointment);
+
+    Console.WriteLine($"{validation.GetType()} result: {result}");
+}
+
+class Appointment
+{
+    public DateTime PatientBirthDate { get; set; }
+    public bool IsVip { get; set; }
+} 
+interface  IValidation
+{
+    public bool Validate(Appointment appointment);
+}
+
+class IsAdultValidation : IValidation
+{
+    public bool Validate(Appointment appointment)
+    {
+        return (DateTime.Now.Year - appointment.PatientBirthDate.Year >= 18);
+    }
+}
+
+class IsVipValidation : IValidation
+{
+    public bool Validate(Appointment appointment)
+    {
+        return appointment.IsVip;
+    }
+}
+
