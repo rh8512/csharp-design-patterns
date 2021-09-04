21f3098dd56de3c0fccedc3a27eb06fe2fa0e5ac
rh8512 <ppenza@gmail.com>
2021-09-04 12:57:52 +0200

    NullObjectPattern

---
+++ DesignPatterns/DesignPatterns.NullObjectPattern/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+IAppointment appointmentNo1 = AppointmentManager.GetAppointment(1);
+IAppointment appointmentNo2 = AppointmentManager.GetAppointment(2);
+IAppointment appointmentNo6 = AppointmentManager.GetAppointment(6);
+
+
+appointmentNo1.CancelAppointment();
+appointmentNo2.CancelAppointment();
+appointmentNo6.CancelAppointment();
+
+
+public interface IAppointment
+{
+    void CancelAppointment();
+}
+
+public class NullAppointment : IAppointment
+{
+    public void CancelAppointment()
+    {
+        //Hands Free, doing nothing is also doing
+        Console.WriteLine("Nothing to cancel");
+    }
+}
+
+public class Appointment : IAppointment
+{
+    public void CancelAppointment()
+    {
+        Console.WriteLine("Appointment canceled");
+    }
+}
+
+public class AppointmentManager
+{
+    public static IAppointment GetAppointment(int id)
+    {
+        if (id > 0 && id < 5) return new Appointment();
+        else return new NullAppointment();
+    }
+}
