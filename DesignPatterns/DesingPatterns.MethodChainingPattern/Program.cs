39514aee4cfd268bea8c3263f877ef26f277e5e5
piotrpenza <ppenza@gmail.com>
2022-02-02 20:46:55 +0100

    Method chaining pattern

---
+++ DesignPatterns/DesingPatterns.MethodChainingPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿AppointmentChain appointmentChain = new AppointmentChain().ScheduleFor("Piotr Penza").SetScheduleDate(DateTime.UtcNow);
+
+
+public class AppointmentChain
+{
+    private Appointment appointment = new();
+
+    private class Appointment
+    {
+        public string? PatientName;
+        public DateTime? AppointmentDate;
+    }
+
+    public AppointmentChain ScheduleFor(string patientName)
+    {
+        appointment.PatientName = patientName;
+        return this;
+    }
+
+    public AppointmentChain SetScheduleDate(DateTime scheduleDate)
+    {
+        appointment.AppointmentDate = scheduleDate;
+        return this;
+    }
+}
